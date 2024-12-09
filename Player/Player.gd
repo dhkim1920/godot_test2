1882a1234e65f13da5895fd78059fcab887b511c
dhkim1920 <dhkim1920@gmail.com>
2024-12-09 22:34:49 +0900

    플레이어, 스태틱바디 컬리전 테스트

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,16 +3,21 @@ extends CharacterBody2D
 func _ready():
 	pass
 
+const ACCELERATION = 400
+const MAX_SPEED = 100
+const FRICTION = 400
 
 func _physics_process(delta: float) -> void:
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-	move_and_collide(velocity)
+	# move_and_collide(velocity * delta)
+	move_and_slide()
